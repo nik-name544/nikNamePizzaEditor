51b3ae0b9ec7422ea10e60edf720479dff6f3999
nik-name544 <65399330+nik-name544@users.noreply.github.com>
2020-12-13 18:30:58 +0200

    Update App.js

--- mainApp/src/App.js
+++ mainApp/src/App.js
@@ -9,9 +9,17 @@ import OrderList from './OrderList/OrderList';
 import PaymentForm from './PaymentForm/PaymentForm';
 import PizzaEditorForm from './PizzaEditor/PizzaEditorForm'
 import './PizzaEditorStyle.css' 
-
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const Registered = useSelector(state => state.Registered);
+  const auth = () => {
+    if (Registered.registered) {
+      return "/order-list"
+    } else {
+      return "/login"
+    }
+  }
   return (
     <>
       <nav>
@@ -20,7 +28,7 @@ function App() {
           <li><Link to="/checkout">Checkout</Link></li>
           <li><Link to="/registration">Registration</Link></li>
           <li><Link to="/login">Login</Link></li>
-          <li><Link to="/order-list">OrderList</Link></li>
+          <li><Link to={auth}>OrderList</Link></li>
           <li><Link to="/payment-form">PaymentForm</Link></li>
         </ul>
       </nav>
